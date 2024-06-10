7da85e9586a4737e47ba1b7539b996cb5f56964c
Emir Medetbekov <klerni89@gmail.com>
2024-06-10 20:55:10 +0600

    fixed bash script

--- docker/entrypoints/start_back.sh
+++ docker/entrypoints/start_back.sh
@@ -2,16 +2,11 @@
 
 run_alembic() {
   local command="$1"
-  alembic --config src/alembic.ini $command
+  alembic --config src/alembic/alembic.ini $command
 }
 
-run_alembic check || run_alembic revision --autogenerate
+run_alembic "check" || run_alembic "revision --autogenerate"
 
-if ! run_alembic "upgrade head"; then
-  echo "Failed to apply migrations"
-  exit 1
-else
-  echo "Migrations were applied"
-fi
+run_alembic "upgrade head"
 
 uvicorn src.main:app --reload --host 0.0.0.0 --port 8000
